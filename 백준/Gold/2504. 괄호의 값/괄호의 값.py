e59bf16272e509dc29785f0d327c37bc710982ef
Jeonghwan Lee <56388433+brian6484@users.noreply.github.com>
2023-10-08 15:51:55 +0900

    [Gold V] Title: 괄호의 값, Time: 40 ms, Memory: 31256 KB -BaekjoonHub

---
+++ 백준/Gold/2504. 괄호의 값/괄호의 값.py
@@ -0,0 +1,35 @@
+stack = [] # 스택
+res = 1 # result에 더해주기 전 임시 변수
+result = 0 # 결과 변수
+p = list(input()) # 입력값
+
+# 1~4번째 과정 시작
+for i in range(len(p)):
+  if p[i]=='(':
+    res *= 2
+    stack.append(p[i])
+    
+  elif p[i]=='[':
+    res *= 3
+    stack.append(p[i])
+    
+  elif p[i]==')':
+    if not stack or stack[-1]!='(':
+      result = 0
+      break
+    if p[i-1]=='(': result += res
+    res //= 2
+    stack.pop()
+    
+  elif p[i]==']':
+    if not stack or stack[-1]!='[':
+      result = 0
+      break
+    if p[i-1]=='[': result += res
+    res //= 3
+    stack.pop()
+
+if stack:
+  print(0)
+else:
+  print(result)
