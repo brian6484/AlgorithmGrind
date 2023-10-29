d32d695e155dd4b2767e2562be3e12595eacad46
Jeonghwan Lee <56388433+brian6484@users.noreply.github.com>
2023-10-29 18:17:03 +0900

    [Silver I] Title: 격자상의 경로, Time: 44 ms, Memory: 31120 KB, Score: 100 point -BaekjoonHub

--- 백준/Silver/10164. 격자상의 경로/격자상의 경로.py
+++ 백준/Silver/10164. 격자상의 경로/격자상의 경로.py
@@ -1,53 +1,29 @@
-import sys
-input = sys.stdin.readline
-from collections import defaultdict, deque
-import math
-
 n,m,z = map(int,input().split())
-graph = [[0 for _ in range(m+1)] for _ in range(n+1)]
-count=1
-for i in range(1,n+1):
-    for j in range(1,m+1):
-        graph[i][j]=count
-        count+=1
-flag = False
-row = z//m +1
-col = z%m
+dp=[[0 for _ in range(m)] for _ in range(n)]
 if z==0:
-    flag=True
-ans = 0
-dx = [1,0]
-dy = [0,1]
-visited= [[False for _ in range(m+1)] for _ in range(n+1)]
-ans_path = []
-path =[]
-
-def dfs(x,y):
-    global ans
-    global flag
-    if x==n and y==m and flag==True:
-        ans+=1
-        ans_path.append(path.copy())
-        return
-    for i in range(2):
-        next_x = dx[i]+x
-        next_y = dy[i]+y
-        if 1<=next_x<=n and 1<=next_y<=m:
-            if not visited[next_x][next_y]:
-                if graph[next_x][next_y]==z:
-                    flag = True
-                visited[next_x][next_y]=True
-                path.append([next_x,next_y])
-                dfs(next_x,next_y)
-                visited[next_x][next_y]=False
-                path.pop()
-                if flag:
-                    flag = False
-                if z==0:
-                    flag=True
-
-
-visited[1][1]=False
-dfs(1,1)
-print(ans)
-# print(ans_path)
+    for i in range(n):
+        for j in range(m):
+            if i==0 or j==0:
+                dp[i][j]=1
+            else:
+                dp[i][j] = dp[i-1][j] + dp[i][j-1]
+    print(dp[n-1][m-1])
+else:
+    x,y = (z-1)//m , (z-1)%m
+    for i in range(x+1):
+        for j in range(y+1):
+            if i==0 or j==0:
+                dp[i][j]=1
+            else:
+                dp[i][j] = dp[i-1][j] + dp[i][j-1]
+    for i in range(x,n):
+        for j in range(y,m):
+            if i==x and j==y:
+                continue
+            if i==x:
+                dp[i][j]=1
+            elif j==y:
+                dp[i][j]=1
+            else:
+                dp[i][j] = dp[i-1][j] + dp[i][j-1]
+    print(dp[x][y] * dp[n-1][m-1])
