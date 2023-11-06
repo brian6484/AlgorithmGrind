a48ff4ce5e96e52609bf296d43796f92365a324b
Jeonghwan Lee <56388433+brian6484@users.noreply.github.com>
2023-11-06 13:41:15 +0900

    [Silver II] Title: 결혼식, Time: 472 ms, Memory: 34060 KB -BaekjoonHub

---
+++ 백준/Silver/5567. 결혼식/결혼식.py
@@ -0,0 +1,30 @@
+from collections import defaultdict
+
+n = int(input())
+m = int(input())
+graph = defaultdict(list)
+
+for _ in range(m):
+    a, b = map(int, input().split())
+    graph[a].append(b)
+    graph[b].append(a)
+
+visited = [False for _ in range(n + 1)]
+ans = [False for _ in range(n + 1)]
+visited[1] = True
+
+def dfs(node, depth):
+    if depth == 2:
+        return
+    for a in graph[node]:
+        if not visited[a]:
+            ans[a] = True
+            visited[a] = True
+            dfs(a, depth + 1)
+            visited[a] = False
+dfs(1,0)
+count = 0
+for i in ans:
+    if i:
+        count += 1
+print(count)
