31575e2dddc71d9cf99fd41d4790cac59ffbe9cb
Jeonghwan Lee <56388433+brian6484@users.noreply.github.com>
2023-11-16 23:55:55 +0900

    [Silver I] Title: IOIOI, Time: 412 ms, Memory: 32368 KB, Score: 100 point -BaekjoonHub

--- 백준/Silver/5525. IOIOI/IOIOI.py
+++ 백준/Silver/5525. IOIOI/IOIOI.py
@@ -1,12 +1,21 @@
+import sys
+input = sys.stdin.readline
+
 n = int(input())
 m = int(input())
 hola = input()
 count = 0
-check = "I" + "OI" * n
-length = len(check)
-
-for i in range(m - length + 1):
-    if hola[i:i + length] == check:
-        count += 1
+i=0
+tmp=0
+while i<len(hola):
+    if hola[i:i+3]=='IOI':
+        tmp+=1
+        i+=2
+        if tmp==n:
+            count+=1
+            tmp-=1
+    else:
+        i+=1
+        tmp=0
 
 print(count)
