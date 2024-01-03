5394216a73caebd681a349b320bc51e0c3f804cd
Jeonghwan Lee <56388433+brian6484@users.noreply.github.com>
2024-01-03 16:50:19 +0900

    [Silver I] Title: 떡 먹는 호랑이, Time: 200 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Silver/2502. 떡 먹는 호랑이/떡 먹는 호랑이.py
+++ 백준/Silver/2502. 떡 먹는 호랑이/떡 먹는 호랑이.py
@@ -1,17 +1,19 @@
-import sys
-d, k = map(int, sys.stdin.readline().split())
+d, k = map(int, input().split())
+dp = [(0, 0) for _ in range(d + 1)]
+dp[1] = (0, 1)
+dp[2] = (1, 0)
 
-for i in range(1, 100000):
-    for j in range(1, 100000):
-        dp = [0] * (d + 1)
-        dp[1] = i
-        dp[2] = j
+for i in range(3, d + 1):
+    dp[i] = (dp[i - 1][0] + dp[i - 2][0], dp[i - 1][1] + dp[i - 2][1])
 
-        for l in range(3, d + 1):
-            dp[l] = dp[l - 1] + dp[l - 2]
+a, b = dp[d]
 
-        if dp[-1] == k:
-
-            print(dp[1])
-            print(dp[2])
-            exit()
+for x in range(1, 100001):
+    for y in range(1, 100001):
+        if a * y + b * x == k:
+            print(x)
+            print(y)
+            break
+    else:
+        continue
+    break
