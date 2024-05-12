5d82159165ef6a549565d117d59674f1a8fb4d49
Jeonghwan Lee <56388433+brian6484@users.noreply.github.com>
2024-05-12 13:03:13 +0900

    [Gold V] Title: A와 B 2, Time: 44 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Gold/12919. A와 B 2/A와 B 2.py
+++ 백준/Gold/12919. A와 B 2/A와 B 2.py
@@ -1,19 +1,15 @@
-s = input()
-t = input()
+s = input().strip()
+t = input().strip()
 
-def dfs(s, t):
-    if len(s) == len(t):
-        if s == t:
-            print(1)
-            exit()
-        else:
-            return
-
-    if t[-1] == 'A':
+def dfs(s,t):
+    if s==t:
+        print(1)
+        exit()
+    if len(s)==len(t):
+        return
+    if t[-1]=='A':
         dfs(s, t[:-1])
-
-    if t[0] == 'B':
-        dfs(s, t[1:][::-1])
-
-dfs(s, t)
+    if t[0]=='B':
+        dfs(s,t[1:][::-1])
+dfs(s,t)
 print(0)
