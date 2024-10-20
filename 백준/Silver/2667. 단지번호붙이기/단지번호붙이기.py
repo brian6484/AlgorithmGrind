a91aaf869922578394f4848032caab6d23b5c315
Jeonghwan Lee <56388433+brian6484@users.noreply.github.com>
2024-10-20 23:58:23 +0900

    [Silver I] Title: 단지번호붙이기, Time: 32 ms, Memory: 31212 KB -BaekjoonHub

--- 백준/Silver/2667. 단지번호붙이기/단지번호붙이기.py
+++ 백준/Silver/2667. 단지번호붙이기/단지번호붙이기.py
@@ -1,29 +1,29 @@
-n=int(input())
-graph=[]
-for _ in range(n):
-    split = input().strip()
-    graph.append(list(map(int,split)))
+import sys
+input = sys.stdin.readline
 
-moves=[[1,0],[-1,0],[0,1],[0,-1]]
+t = int(input())
+graph=[]
+for _ in range(t):
+    graph.append(list(map(int,input().strip())))
+moves = [[1,0],[-1,0],[0,1],[0,-1]]
 ans=[]
-count=0
-def dfs(x,y):
+count = 0
+
+def dfs(i,j):
     global count
     count+=1
+    graph[i][j]=0
     for move in moves:
-        next_x, next_y = move[0]+x, move[1]+y
-        if 0<=next_x<n and 0<=next_y<n:
-            if graph[next_x][next_y]==1:
-                graph[next_x][next_y]=0
-                dfs(next_x,next_y)
+        next_x,next_y = move[0]+i,move[1]+j
+        if 0<=next_x<len(graph) and 0<=next_y<len(graph[0]) and graph[next_x][next_y]==1:
+            dfs(next_x,next_y)
 
-for i in range(n):
-    for j in range(n):
+for i in range(t):
+    for j in range(len(graph[0])):
         if graph[i][j]==1:
-            graph[i][j]=0
             dfs(i,j)
             ans.append(count)
-            count =0
+            count = 0
 
 ans.sort()
 print(len(ans))
