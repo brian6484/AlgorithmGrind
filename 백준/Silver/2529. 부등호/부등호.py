72af406cc90a0673e3a2f3ae06220a5f5bb7bcda
Jeonghwan Lee <56388433+brian6484@users.noreply.github.com>
2024-11-24 23:18:10 +0900

    [Silver I] Title: 부등호, Time: 116 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Silver/2529. 부등호/부등호.py
+++ 백준/Silver/2529. 부등호/부등호.py
@@ -1,33 +1,33 @@
-n = int(input())
-brackets = list(map(str,input().split()))
-visited= [False for _ in range(10)]
-min_ans=''
-max_ans=''
+n=int(input())
+brackets=list(map(str,input().split()))
+visited=[False for _ in range(10)]
+min_ans,max_ans=0,0
+ans=[]
+
 def check(i,j,h):
     if h=='<':
         return i<j
     else:
         return i>j
 
-def dfs(idx,hola):
-    global min_ans,max_ans
-    if idx==n+1:
-        if len(min_ans)==0:
-            # since we are iterating from 0 to 9 sequentially the first string that meets the condition is garanteed the smallest
-            min_ans=hola
+def dfs():
+    global ans,min_ans,max_ans
+    if len(ans)==n+1:
+        if min_ans==0:
+            min_ans = ''.join(map(str,ans))
         else:
-            max_ans=hola
+            max_ans = ''.join(map(str,ans))
         return
 
     for i in range(10):
         if not visited[i]:
-            if idx==0 or check(hola[-1],str(i),brackets[idx-1]):
-                # if i do hola+=str(i), I need to slice it for backtracking or else it will accumulate
-                hola+=str(i)
-                visited[i]=True
-                dfs(idx+1,hola)
-                hola=hola[:-1]
-                visited[i]=False
-dfs(0,'')
+          if not ans or check(ans[-1],i,brackets[len(ans)-1]):
+              ans.append(i)
+              visited[i]=True
+              dfs()
+              visited[i]=False
+              ans.pop()
+
+dfs()
 print(max_ans)
 print(min_ans)
