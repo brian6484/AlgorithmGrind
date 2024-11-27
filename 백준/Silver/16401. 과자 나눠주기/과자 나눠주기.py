d3a15de9f460a5063c96de11b1c31f8d423bb7fb
Jeonghwan Lee <56388433+brian6484@users.noreply.github.com>
2024-11-27 20:52:56 +0900

    [Silver II] Title: 과자 나눠주기, Time: 3024 ms, Memory: 142104 KB -BaekjoonHub

--- 백준/Silver/16401. 과자 나눠주기/과자 나눠주기.py
+++ 백준/Silver/16401. 과자 나눠주기/과자 나눠주기.py
@@ -3,9 +3,9 @@ input = sys.stdin.readline
 
 m,n=map(int,input().split())
 lst=list(map(int,input().split()))
-left,right=1,max(lst)
+left,right=1,max(lst)+1
 ans=0
-while left<=right:
+while left<right:
     mid = left + (right-left)//2
     total=0
     for i in lst:
@@ -14,5 +14,5 @@ while left<=right:
         ans=max(ans,mid)
         left=mid+1
     else:
-        right=mid-1
-print(ans)
+        right=mid
+print(left-1)
